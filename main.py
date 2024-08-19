8596cbf648d125940b8deb09c4defc5cd3e3898a
Maja <153939375+Vinde2@users.noreply.github.com>
2024-08-19 13:20:02 +0200

    Update main.py[quiz and news]

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os, random
 import requests
 from model import get_class
-from checkname import check_extention
+from checkname import check_extention, Random_Question, Quiz #Answering
 
 
 intents = discord.Intents.default()
@@ -15,6 +15,8 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print(f'Zalogowaliśmy się jako {bot.user}')
 
+
+
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Cześć! Jestem botem, {bot.user}!')
@@ -22,6 +24,11 @@ async def hello(ctx):
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+
+@bot.command()
+async def news(ctx):
+    await ctx.send(f"Oto losowa ciekawostka:\n{Random_Question()}")
+
 
 @bot.command()
 async def check(ctx):
@@ -36,8 +43,22 @@ async def check(ctx):
     else :
         await ctx.send("Nothing here")
 
+@bot.command()
+async def quiz(ctx):
+    await ctx.send(f"Oto losowe pytanie\n{Quiz()}")
+    # if ctx.message.attachments :
+    #     answer = ctx.message.attachments 
+    #     Answering(answer)
+    
+@bot.command()
+async def comands(ctx):
+    await ctx.send("komendy:\n hello - przywitaj się z botem \n news - losowe ciekawostki \n quiz - chce wziąść udział w quizie z ciekawostek \n check - możesz zrobić zdięcie elektrowni por ekologicznej a sztuczna inteligencja powiadomi się co to za elektrownia:)")
 
 
 
-bot.run("MTE5Mjg3MTc2ODU5NjE2MDU1Mg.G3Cs97.GZrQAJp1V4aAMDTgKvMu7JgZ8dPQ6VGdKy7Dgs")
+
+
+
+
+bot.run("SECRET TOKEN")
 
