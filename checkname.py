7ae6a9bcc9a135d5daf933eae1c71b7a65caf2c6
Maja <153939375+Vinde2@users.noreply.github.com>
2024-08-20 14:43:07 +0200

    Update checkname.py
    some additional changes

--- checkname.py
+++ checkname.py
@@ -20,17 +20,20 @@ List = [
     "6.Baterie grawitacyjne - Przechowywuje energię elektryczną, Zazwyczaj są w postaci wielkiego dźwigu z obciążnikami rzadziej występują w starych kopalniach ",
     "7.Odnawialne źródła energii – źródła energii, których wykorzystywanie nie wiąże się z długotrwałym ich deficytem, ponieważ ich zasób odnawia się w relatywnie krótkim czasie",
     "8.nieodnawialne źródła energii, czyli źródła, których zasoby odtwarzają się bardzo powoli bądź wcale: ropa naftowa, węgiel, gaz ziemny i uran pozyskiwany z kopalin.",
-    "9.TU BĘDZIE LINK DO QUIZU"
-
-
-
-
-
-
+    "9.Link dla osób które chciałyby uzupełnićswoją wiedze na temat energi odnawialnej:https://www.eea.europa.eu/pl/sygna142y/sygnaly-2022/artykuly/przyszlosc-oparta-na-energii-odnawialnej"
+    "10.Bardzo Interesujący pomysł reaktorów IV generacji: https://pl.wikipedia.org/wiki/Reaktor_post%C4%99puj%C4%85cej_fali"
 
 ]
 
 
+
+# List = random.shuffle(List)
+
+# def Random_Question():
+#     List = random.shuffle(List)
+#     norandom = List[0]
+#     return norandom
+#     #return List[1]
 def Random_Question():
     random.shuffle(List)
 
@@ -39,23 +42,25 @@ def Random_Question():
         
 
 level1 = {
-        'Co oznacz skrót ESP?' : 'elektrownia szczytowo-pompowa',
-        'Jak nazywamy : źródła energii, których wykorzystywanie nie wiąże się z długotrwałym ich deficytem, ponieważ ich zasób odnawia się w relatywnie krótkim czasie' : 'odnawialene źródła energi'
-
-
-
-
-
+        '1.Co oznacz skrót ESP?' : 'elektrownia szczytowo-pompowa',
+        '2.Jak nazywamy : źródła energii, których wykorzystywanie nie wiąże się z długotrwałym ich deficytem, ponieważ ich zasób odnawia się w relatywnie krótkim czasie' : 'odnawialene źródła energi',
+        '3.jaki jest synonim do nazwy elektroenia jądrowa?' : 'elektrownia atomowa',
+        '4.czy elektrownie jądrowe są odnawialnym źródłem energi?' : 'Nie , Uran podobnie jak węgiel jest wydobywany z kopalni.',
+        '5.Podaj przykład nowoczesnej formy przetrzymywania energi' : 'np. Baterie grawitacyjne',
+        '6.Podaj 4 przykłady odnawialnych źródeł energi' : 'np. elektrownia - słoneczna - wiatrowa - wodna -geotermalna'
 
 }
 
 def Quiz():
     levelend = level1.keys()
-    x = random.randint(0,1)
+    x = random.randint(0,5)
     levelland = list(levelend)
     return {levelland[x]}
     
-print(Quiz())
+def The_End():
+    answer = level1
+    return answer
+
     
 # def Answering(answer):    
 #     levelend = level1.keys()
@@ -65,6 +70,8 @@ print(Quiz())
 #          return botanswer
 #     else :
 #          botanswer = "Zła odpowiedź"
+
+
 
 
 
